9ebfffdf3370a70a4730c3e0cd667ab0ceb1a264
mait <mait@fenomen.ee>
2015-09-18 08:32:18 +0300

    update

--- scripts/service/service.js
+++ scripts/service/service.js
@@ -2,21 +2,21 @@ var api = 'http://mait.fenomen.ee:4040/';
 app.service('testService', function ($http) {
   this.getGames = function () {
     return $http.get(api + 'games');
-  }
+  };
 
-  this.addGame = function() {
-    return $http.get(api + 'creategame')
-  }
+  this.addGame = function(players) {
+    return $http.post(api + 'newgame', players);
+  };
 
   this.getGame = function(id) {
     return $http.get(api + 'game/' + id);
-  }
+  };
 
   this.addGoal = function(goal) {
     return $http.post(api + 'addgoal', goal);
-  }
+  };
 
   this.getTeam = function(id) {
     return $http.get(api + 'team/' + id);
-  }
+  };
 });
